f10d97d5a6647bf68e2bd1398e4b5fd907c0758a
BertoCruz <berto.lopezcruz@coloradocollege.edu>
2015-04-29 08:58:22 -0600

    Create wget.js

---
+++ wget.js
@@ -0,0 +1,9 @@
+/*
+I didn't get a chance to finish yesterday's work, with my thesis proposal due yeasterday and meetings and work I wasn't able to finish.
+however, the assignment heavily entails in being able to read a file that contains both the url link and the destination in which to 
+save those files. The wget.js file would first off be able to read this file that contains both of these elements. Second, the function
+would need to be able to read two ines at a time, so as to sepearte the url line from the destination source, and be able to save it into an array.
+once the destination files and the url files are saved in an array, we would then be able to read each index, one by one, in order to feed
+it into our "download" functon, that takes in the url source and destination source. Simple as that :)
+
+-Roberto
